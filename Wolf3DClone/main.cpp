08e19a6eaa01b32dea8f5182297f3fcd81c2654f
MarcusColey <marcus.09@live.com>
2024-08-27 21:54:05 -0700

    Initial Commit

---
+++ Wolf3DClone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Wolf3DClone
+//
+//  Created by Marcus on 2024-08-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
